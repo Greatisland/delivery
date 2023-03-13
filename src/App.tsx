ced2298a87341c8e4fc6fe526d71f6152af264f9
Hyeon JIN <dontsendloveletters@gmail.com>
2023-03-13 08:59:16 +0000

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <p>택배 받아오기</p>
+//       <form></form>
+//     </div>
+//   )
+// }
+
+// export default App
+import { useEffect, useState } from 'react'
+
+interface CompanyType {
+  Code: string;
+  International: string;
+  Name: string;
+}
+
+interface FirstCom {
+  Company: CompanyType[];
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [companyList, setCompanyList] = useState<FirstCom>()
+
+
+
+
+  useEffect(() => {
+    getCompanyList()
+  },[])
+
+
+  
+  const getCompanyList = async() => {
+    const data = await fetch("https://info.sweettracker.co.kr/api/v1/companylist?t_key=fwM3wePoMWuqF5k3n1f30Q")
+    const json = await data.json()
+    console.log(json)
+
+    setCompanyList(json)
+  }
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      프로젝트임
+      
+      <select name="" id="">
+
+        {companyList?.Company?.map((data, i) => (
+          <option key={i}>{data.Name}</option>
+        ))}
+      </select>
+      
     </div>
   )
 }
